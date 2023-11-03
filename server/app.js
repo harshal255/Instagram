f72140f4d4ef6488c7fcf40cb0fd4914247999c5
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-11-03 22:15:28 +0530

    mongodb-connection-dotenv-bcypt ⚡🔒➕

--- server/app.js
+++ server/app.js
@@ -3,11 +3,18 @@ const app = express()
 const port = 5000;
 const data = require('./data')
 const cors = require('cors')
-
+const mongoose = require('mongoose');
+require('dotenv').config();
+require('./models/model')
 // if you don't want to show cros error then use cors-express
 app.use(cors())
 
+//middleware:Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application’s request-response cycle. The next middleware function is commonly denoted by a variable named next.
+app.use(express.json());
+
 //Availabe Routes
+app.use(require("./routes/auth"));
+
 app.get('/', (req, res) => {
     res.json(data);
 })
@@ -15,5 +22,13 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port} : http://localhost:${port}`)
+    // console.log(process.env.DB_URL)
+    mongoose.connect(process.env.DB_URL);
+    mongoose.connection.on("connected", () => {
+        console.log("successfully connected to mongo")
+    })
+    mongoose.connection.on("error", () => {
+        console.log("not connected to mongo")
+    })
 
 })
