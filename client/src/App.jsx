722c96b8b88bf6acd3c4660d4541f78986dc272c
harshal255 <200280107119@ldceahm.gujgov.edu.in>
2023-11-09 02:27:29 +0530

    material-tailwind conf 🔧

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import Loginpage from './pages/Loginpage';
 import Home from './pages/Home';
 import Signuppage from './pages/Signuppage';
 import { Routes, Route } from 'react-router-dom'
 
 function App() {
+
+
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home></Home>}> </Route>
+        <Route path='/login' element={<Loginpage></Loginpage>}> </Route>
         <Route path='/signup' element={<Signuppage></Signuppage>}></Route>
+        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
     </>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import Login from "../components/Login"
-import Mobilescreen from "../components/Mobilescreen"
-
+import { Button } from "@material-tailwind/react";
 
 const Home = () => {
     return (
-        <div className="flex w-[100vw] h-[100vh] justify-evenly items-center md:px-64 border-2 border-red-500">
-            <Mobilescreen></Mobilescreen>
-            <Login></Login>
-        </div>
+        <div><Button>I love you</Button></div>
     )
 }
 
